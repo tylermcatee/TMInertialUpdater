64932cc216f753d2eebe1e0d469600cb7a1ad219
Tyler McAtee <tylermcatee@apple.com>
2016-11-11 15:12:31 -0800

    Initial Commit

---
+++ TMInertialUpdater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TMInertialUpdater
+//
+//  Created by Tyler McAtee on 11/11/16.
+//  Copyright © 2016 McAtee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TMInertialUpdater/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TMInertialUpdater
+//
+//  Created by Tyler McAtee on 11/11/16.
+//  Copyright © 2016 McAtee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
